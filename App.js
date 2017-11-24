594b5a9a7112c346d16edbd864bd5baa5759d49e
robertpaulmarinko@yahoo.com <robertpaulmarinko@live.com>
2017-11-23 20:55:25 -0500

    Created Home and Game screens.

--- App.js
+++ App.js
@@ -1,23 +1,41 @@
 import React from 'react';
 import { Alert, Button, StyleSheet, Text, View } from 'react-native';
+import {
+  StackNavigator,
+} from 'react-navigation';
 
-export default class App extends React.Component {
-  _onPressButton() {
-    Alert.alert('You tapped the button!')
-  }
-  
+export class HomeScreen extends React.Component {
   render() {
+    // props.navigation is automatically set by the StackNavigator
+    const { navigate } = this.props.navigation;
     return (
       <View style={styles.container}>
         <Text>Welcome to React Adventure!</Text>
         <Button
-            onPress={this._onPressButton}
-            title="Start Game"
-          />
+          onPress={() => navigate('Game')}
+          title="Start Game"
+        />          
       </View>
     );
   }
 }
+
+export class GameScreen extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>GAME GOES HERE</Text>
+      </View>
+    );
+  }
+}
+
+const App = StackNavigator({
+  Home: { screen: HomeScreen },
+  Game: { screen: GameScreen },
+});
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
